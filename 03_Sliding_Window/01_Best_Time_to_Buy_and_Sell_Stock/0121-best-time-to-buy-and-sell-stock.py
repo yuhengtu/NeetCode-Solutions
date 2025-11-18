16ff326c09a5a26c5d3268d03a3bd27c4cf3ddda
yuhengtu <145947541+yuhengtu@users.noreply.github.com>
2025-11-19 07:20:27 +0800

    Update 0121-best-time-to-buy-and-sell-stock.py

--- 03_Sliding_Window/01_Best_Time_to_Buy_and_Sell_Stock/0121-best-time-to-buy-and-sell-stock.py
+++ 03_Sliding_Window/01_Best_Time_to_Buy_and_Sell_Stock/0121-best-time-to-buy-and-sell-stock.py
@@ -14,16 +14,13 @@ The space complexity is O(1) since we are not using any additional data structur
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0, 1
-        max_p = 0
-        
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                max_p = max(max_p, profit)
+        l = 0
+        max_profit = 0
+        for r in range(len(prices)):
+            profit = prices[r] - prices[l]
+            if profit >= 0:
+                max_profit = max(max_profit, profit)
             else:
                 l = r
             
-            r += 1
-        
-        return max_p
+        return max_profit
