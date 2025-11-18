89b7be5ad6f5b2e295807359be7cb2dd8c3cf3ce
yuhengtu <145947541+yuhengtu@users.noreply.github.com>
2025-11-19 07:28:59 +0800

    Update 0003-longest-substring-without-repeating-characters.py

--- 03_Sliding_Window/02_Longest_Substring_Without_Repeating_Characters/0003-longest-substring-without-repeating-characters.py
+++ 03_Sliding_Window/02_Longest_Substring_Without_Repeating_Characters/0003-longest-substring-without-repeating-characters.py
@@ -14,17 +14,13 @@ The space complexity is O(k), where k is the number of unique characters in the 
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left, right = 0, 0
-        max_length = 0
-        unique_chars = set()
-
-        while right < len(s):
-            if s[right] not in unique_chars:
-                unique_chars.add(s[right])
-                max_length = max(max_length, right - left + 1)
-                right += 1
-            else:
-                unique_chars.remove(s[left])
-                left += 1
-
-        return max_length
+        seen = set()
+        max_len = 0
+        l = 0
+        for r in range(len(s)):
+            while s[r] in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(s[r])
+            max_len = max(max_len, r-l+1)
+        return max_len
